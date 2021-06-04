60a3453bbe5d8a9d2d3603d118ba5855fa29e2f9
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2021-06-04 13:33:56 +0200

    Restrict Doorlock state propery values

--- app/models/doorlock.rb
+++ app/models/doorlock.rb
@@ -1,3 +1,4 @@
 class Doorlock < ApplicationRecord
   validates_presence_of :state
+  validates_inclusion_of :state, in: %w[open locked]
 end
--- spec/models/doorlock_spec.rb
+++ spec/models/doorlock_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Doorlock, type: :model do
-  it { should validate_presence_of(:state) }
+  it { is_expected.to validate_presence_of(:state) }
+
+  it {
+    is_expected.to validate_inclusion_of(:state)
+      .in_array(%w[open locked])
+  }
 end
