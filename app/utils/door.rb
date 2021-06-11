cba981771f7ae69642feb3fd7d84f883223ff361
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2021-06-11 16:04:35 +0200

    Remove begin block for testing

--- app/utils/door.rb
+++ app/utils/door.rb
@@ -3,14 +3,10 @@ class Door
     RPi::GPIO.set_numbering :board
     RPi::GPIO.setup 11, as: :input, pull: :up
 
-    begin
-      if RPi::GPIO.high? 11
-        Doorlock.create!(state: 'open')
-      else
-        Doorlock.create!(state: 'locked')
-      end
-    rescue StandardError
-      RPi::GPIO.clean_up
+    if RPi::GPIO.high? 11
+      Doorlock.create!(state: 'open')
+    else
+      Doorlock.create!(state: 'locked')
     end
   end
 end
--- spec/app/utils/door_spec.rb
+++ spec/app/utils/door_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Door do
-  subject(:check_lock_state) { described_class.check_lock_state }
+  describe '#check_lock_state' do
+    subject(:check_lock_state) { described_class.check_lock_state }
 
-  context 'when door is open' do
-    before { allow(RPi::GPIO).to receive(:high?).and_return true }
+    context 'when door is open' do
+      before { allow(RPi::GPIO).to receive(:high?).and_return true }
 
-    it 'saves the open state of the door lock' do
-      expect(Doorlock).to receive(:create!).with(state: 'open')
-      expect { check_lock_state }.to change(Doorlock.all, :size).by(1)
+      it 'saves the open state of the door lock' do
+        expect(Doorlock).to receive(:create!).with(state: 'open')
+        expect { check_lock_state }.to change(Doorlock.all, :size).by(1)
+      end
     end
-  end
 
-  context 'when door is closed' do
-    before { allow(RPi::GPIO).to receive(:high?).and_return false }
+    context 'when door is closed' do
+      before { allow(RPi::GPIO).to receive(:high?).and_return false }
 
-    it 'saves the locked state of the door lock' do
-      expect(Doorlock).to receive(:create!).with(state: 'locked')
-      expect { check_lock_state }.to change(Doorlock.all, :size).by(1)
+      it 'saves the locked state of the door lock' do
+        expect(Doorlock).to receive(:create!).with(state: 'locked')
+        expect { check_lock_state }.to change(Doorlock.all, :size).by(1)
+      end
     end
   end
 end
