b8a3cb3eaa46ff9e7ef2a78e1884697a0f9c275c
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2021-06-25 13:04:09 +0200

    Refactor

--- check_lock.rb
+++ check_lock.rb
@@ -3,12 +3,14 @@ require_relative 'bootstrap'
 RPi::GPIO.set_numbering :board
 RPi::GPIO.setup 11, as: :input, pull: :up
 
+state = if RPi::GPIO.high? 11
+  'open'
+else
+  'locked'
+end
+
+message = { state: state, created_at: Time.now }.to_json
+
 MQTT::Client.connect(cloud_mqtt) do |c|
-  state = if RPi::GPIO.high? 11
-            'open'
-          else
-            'locked'
-          end
-  message = { state: state, created_at: Time.now }.to_json
   c.publish('doorlock', message)
 end
