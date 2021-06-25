e6ad00086ec6ff3a9582d88120f527fc268a6eee
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2021-06-25 13:14:01 +0200

    Remove loop not to run out of connections

--- check_message.rb
+++ check_message.rb
@@ -1,11 +1,9 @@
 require_relative 'bootstrap'
 
-loop do
-  Thread.new do
-    MQTT::Client.connect(cloud_mqtt) do |c|
-      c.get('doorlock') do |topic, message|
-        puts "#{topic}: #{message}"
-      end
+Thread.new do
+  MQTT::Client.connect(cloud_mqtt) do |c|
+    c.get('doorlock') do |topic, message|
+      puts "#{topic}: #{message}"
     end
   end
 end
