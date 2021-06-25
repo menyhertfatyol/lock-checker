6c04f50b14faf51ec214a6e0652aaf461dfbd070
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2021-06-25 15:05:36 +0200

    Use the pin_num argument

--- app/utils/door.rb
+++ app/utils/door.rb
@@ -1,9 +1,9 @@
 class Door
   def self.check_lock_state(pin_num: 11)
     RPi::GPIO.set_numbering :board
-    RPi::GPIO.setup 11, as: :input, pull: :up
+    RPi::GPIO.setup pin_num, as: :input, pull: :up
 
-    if RPi::GPIO.high? 11
+    if RPi::GPIO.high? pin_num
       Doorlock.create!(state: 'open')
     else
       Doorlock.create!(state: 'locked')
