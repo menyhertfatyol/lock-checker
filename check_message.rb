cdaec33273e8bef6347acea2b32463b1988751a8
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2021-06-25 13:16:00 +0200

    Try looping in a different place

--- check_message.rb
+++ check_message.rb
@@ -2,8 +2,10 @@ require_relative 'bootstrap'
 
 Thread.new do
   MQTT::Client.connect(cloud_mqtt) do |c|
-    c.get('doorlock') do |topic, message|
-      puts "#{topic}: #{message}"
+    loop do
+      c.get('doorlock') do |topic, message|
+        puts "#{topic}: #{message}"
+      end
     end
   end
 end
